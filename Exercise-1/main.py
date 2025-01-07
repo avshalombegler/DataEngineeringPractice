ab1fc2f50ee7aecb4020c5ecf286e6d367e7de22
Avshalom Begler <avshalom210@gmail.com>
2025-01-07 15:52:05 +0200

    update

--- Exercise-1/main.py
+++ Exercise-1/main.py
@@ -20,21 +20,24 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+def get_zip_files_names():
+    for uri in download_uris:
+        zip_files_names.append(uri.split('/')[-1])
+
 
 def download_files():
     if not os.path.exists(DOWNLOADS_PATH):
         os.mkdir(DOWNLOADS_PATH)
         print("'downloads' folder created.")
 
-    for uri in download_uris:
+    for (uri, zip_file_name) in zip(download_uris, zip_files_names):
         response = requests.get(uri)
-        zip_files_names.append(uri.split('/')[-1])
-        with open(os.path.join(DOWNLOADS_PATH, zip_files_names[-1]), "wb") as uri:
+        with open(os.path.join(DOWNLOADS_PATH, zip_file_name), "wb") as uri:
             uri.write(response.content)
-            print(f"file: {zip_files_names[-1]} downloaded to 'downloads' folder.")
+            print(f"file: '{zip_file_name}' downloaded to 'downloads' folder.")
 
 
-def get_files_names():
+def get_csv_files_names():
     for file in zip_files_names:
         csv_files_names.append(file.split('.')[0] + CSV)
 
@@ -42,30 +45,31 @@ def get_files_names():
 def extract_zip_file():
     try:
         for (zip_file, csv_file) in zip(zip_files_names, csv_files_names):
+            print(f"Extracting file: '{csv_file}' from '{zip_file}' to 'downloads' folder.")
             with zipfile.ZipFile(os.path.join(DOWNLOADS_PATH, zip_file), 'r') as zip_file:
                 zip_file.extract(csv_file, DOWNLOADS_PATH)
-                print(f"file: {csv_file} extracted from {zip_file} to 'downloads' folder.")
 
     except zipfile.BadZipFile:
-        print(f"Error occured when tried to unzip file: {zip_file}")
+        print(f"Error occured when tried to unzip file: '{zip_file}'")
 
     except FileNotFoundError:
-        print(f"File Not Found Error when tried to unzip file: {zip_file}")
+        print(f"File Not Found Error when tried to unzip file: '{zip_file}'")
 
 def delete_zip_files():
     try:
         for zip_file in zip_files_names:
             os.remove(os.path.join(DOWNLOADS_PATH, zip_file))
-            print(f"file: {zip_file} deleted from 'downloads' folder.")
+            print(f"file: '{zip_file}' deleted from 'downloads' folder.")
 
     except FileNotFoundError:
-        print(f"File Not Found Error when tried to unzip file: {zip_file}")
+        print(f"File Not Found Error when tried to unzip file: '{zip_file}'")
 
 
 def main():
 
+    get_zip_files_names()
     download_files()
-    get_files_names()
+    get_csv_files_names()
     extract_zip_file()
     delete_zip_files()
 
